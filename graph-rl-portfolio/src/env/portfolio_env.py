9595349fe919adec3979a7a5c4adf46e66bdc09d
Aravind <kumar.aravind@aol.com>
2026-01-10 00:21:48 +0000

    Add initial project structure with configuration, environment setup, and data analysis notebooks

---
+++ graph-rl-portfolio/src/env/portfolio_env.py
@@ -0,0 +1,30 @@
+# import packages
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+import datetime
+
+from finrl import config
+from finrl import config_tickers
+from finrl.marketdata.yahoodownloader import YahooDownloader
+from finrl.preprocessing.preprocessors import FeatureEngineer
+from finrl.preprocessing.data import data_split
+from finrl.env.environment import EnvSetup
+from finrl.env.EnvMultipleStock_train import StockEnvTrain
+from finrl.env.EnvMultipleStock_trade import StockEnvTrade
+from finrl.model.models import DRLAgent
+from finrl.trade.backtest import BackTestStats, BaselineStats, BackTestPlot, backtest_strat, baseline_strat
+from finrl.trade.backtest import backtest_strat, baseline_strat
+
+import os
+if not os.path.exists("./" + config.DATA_SAVE_DIR):
+    os.makedirs("./" + config.DATA_SAVE_DIR)
+if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
+    os.makedirs("./" + config.TRAINED_MODEL_DIR)
+if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
+    os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
+if not os.path.exists("./" + config.RESULTS_DIR):
+    os.makedirs("./" + config.RESULTS_DIR)
+
